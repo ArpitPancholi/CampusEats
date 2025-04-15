653ad09fa0c3c295f6cc43de5d5f7a87d1a8ef85
ArpitPancholi <122693994+ArpitPancholi@users.noreply.github.com>
2025-04-15 22:12:49 +0530

    Delete backend directory

--- backend/routes/resetPasswordRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/resetPasswordRoutes.js
-const express = require('express');
-const router = express.Router();
-const ResetPasswordController = require('../controllers/ResetPassword');
-
-router.post('/token', ResetPasswordController.resetPasswordToken);
-router.post('/', ResetPasswordController.resetPassword);
-
-module.exports = router;
